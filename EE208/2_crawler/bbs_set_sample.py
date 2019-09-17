59e04418a751e1179150968ef112f0045a6aec5b
ltzone <ltzhou@sjtu.edu.cn>
2019-09-17 17:16:11 +0800

    crawler1 finished

--- EE208/2_crawler/bbs_set_sample.py
+++ EE208/2_crawler/bbs_set_sample.py
@@ -1,7 +1,9 @@
-# -*- coding:utf-8 -*-
+# -*- coding: utf-8 -*-
 import urllib2, cookielib, urllib
 from bs4 import BeautifulSoup
 import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 def bbs_init_cookie():
     cj = cookielib.CookieJar()  # 初始化cookie
@@ -20,14 +22,27 @@ def bbs_login(id, pw):
     return id in response.read()  # True则成功
 
 
+def bbs_write(text):
+    postdata = urllib.urlencode({
+        'type': 'update',
+        'text': text
+    })
+    req = urllib2.Request(url='https://bbs.sjtu.edu.cn/bbsplan', data=postdata)
+    response = urllib2.urlopen(req)
+    return "个人说明档修改成功。" in response.read().decode('gbk').encode('utf-8')
+
 def bbs_set(id, pw, text):
     bbs_init_cookie()
-    print bbs_login(id, pw)
-    '''
+    if bbs_login(id, pw):
+        if bbs_write(text):
+            print "Write Success!"
+        else:
+            print "Write Failed!"
+    else:
+        print "Login Failed!"
     content = urllib2.urlopen('https://bbs.sjtu.edu.cn/bbsplan').read()
-    soup = BeautifulSoup(content)
+    soup = BeautifulSoup(content,features="html.parser")
     print str(soup.find('textarea').string).strip().decode('utf8')
-    '''
 
 if __name__ == '__main__':
 	
