98e1d66999bdc924559306fb2f6c7992ffc68c11
ltzone <ltzhou@sjtu.edu.cn>
2019-09-22 11:02:51 +0800

    IMG v2.0

--- EE208/1_Parser/IMG.py
+++ EE208/1_Parser/IMG.py
@@ -1,5 +1,7 @@
 import sys
 import urllib2
+import urllib
+import urlparse
 import re
 from bs4 import BeautifulSoup
 
@@ -17,15 +19,26 @@ def write_outputs(urls, filename):
             f.write(url)
             f.write('\n')
 
+def URL_set_uniform(URLset,url):
+    urlseg = urlparse.urlparse(url)
+    uniURLs = set()
+    for j in URLset:
+        if re.match('^//',j):
+            uniURLs.add(urlseg.scheme+ ':'+j)
+        elif (not re.match('://',j)):
+            uniURLs.add(urlparse.urljoin(url,j))
+        else:
+            uniURLs.add(j)
+    return uniURLs
 
 def main():
-    url = 'http://www.baidu.com'
-    # url = 'http://www.sjtu.edu.cn'
+    #url = 'http://www.baidu.com'
+    url = 'http://www.sjtu.edu.cn'
     if len(sys.argv) > 1:
         url = sys.argv[1]
     content = urllib2.urlopen(url).read()
     urls = parseIMG(content)
-    write_outputs(urls, 'res2.txt')
+    write_outputs(URL_set_uniform(urls,url), 'res2.txt')
 
 
 if __name__ == '__main__':
--- EE208/1_Parser/try.py
+++ EE208/1_Parser/try.py
@@ -1,2 +1,12 @@
-i = 'aaabass'
-print i.split('c')
+import sys
+import urllib2
+import re
+from bs4 import BeautifulSoup
+
+url = 'http://www.baidu.com'
+# url = 'http://www.sjtu.edu.cn'
+if len(sys.argv) > 1:
+    url = sys.argv[1]
+content = urllib2.urlopen(url).read()
+soup = BeautifulSoup(content,features='html.parser')
+print soup
